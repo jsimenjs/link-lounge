a4dfd65f3a03a46395a793395b11d61b54d6e029
jsimenjs <32468772+jsimenjs@users.noreply.github.com>
2024-05-20 22:54:11 +0200

    feat: support multiple user generated communication hubs

--- server/main.go
+++ server/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-    hub := newHub()
-    go hub.run()
-    http.Handle("/", http.FileServer(http.Dir("public")))
-    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-        serveWs(hub, w, r)
-    })
-    log.Fatal(http.ListenAndServe(":8080", nil))
+type Manager struct {
+	hubs []*Hub
 }
 
+func main() {
+	manager := Manager{}
+	http.HandleFunc("/{id}/ws", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		for _, hub := range manager.hubs {
+			if id == hub.id {
+				serveWs(hub, w, r)
+				return
+			}
+		}
+		hub := newHub(&manager, id)
+		go hub.run()
+		serveWs(hub, w, r)
+		manager.hubs = append(manager.hubs, hub)
+		fmt.Printf("Created new hub with id: %s\n", hub.id)
+	})
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func (m *Manager) print() {
+		fmt.Println("Hubs:")
+		for i, hub := range m.hubs {
+			fmt.Printf("%d: %s\n", i, hub.toString())
+		}
+}
