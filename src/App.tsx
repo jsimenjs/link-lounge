acd05e334007955d09ed9c63bc42ad63176969d4
jsimenjs <32468772+jsimenjs@users.noreply.github.com>
2024-05-24 21:55:59 +0200

    feat: move websocket logic to chat window component

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,11 @@
-import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { ChatEvent } from './types';
 import ChatWindow from './components/ChatWindow';
-import { bytesToBase64 } from './utils';
 
 function App() {
-    const [chatHistory, setChatHistory] = useState<ChatEvent[]>([]);
-    const [ws, setWs] = useState<WebSocket | null>(null)
-    const wsRef = useRef<WebSocket | null>(null)
-    const [roomId, setRoomId] = useState("")
-
-    useEffect(() => {
-        console.log('ws useEffect triggered')
-        if (roomId === "") { return }
-        if (!wsRef.current || wsRef.current.readyState === WebSocket.CLOSED || wsRef.current.readyState === WebSocket.CLOSING) {
-            console.log(`Opening a new ws connection to id: ${roomId}`)
-            const ws = new WebSocket(`ws://localhost:8080/${bytesToBase64(new TextEncoder().encode(roomId))}/ws`);
-            wsRef.current = ws
-            wsRef.current.onmessage = (event) => {
-                //const date = new Date(Date.now())
-                //const timestamp = `[${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}]`
-                const chatEvent: ChatEvent = JSON.parse(event.data)
-                console.log(chatEvent)
-                setChatHistory(chatHistory => chatHistory.concat(chatEvent))
-            }
-
-            wsRef.current.onopen = () => {
-                const chatEvent: ChatEvent = { type: "status", payload: 'Connected.' }
-                setChatHistory(chatHistory => chatHistory.concat(chatEvent))
-            }
-
-            wsRef.current.onclose = () => {
-                const chatEvent: ChatEvent = { type: "status", payload: 'Connection closed.' }
-                setChatHistory(chatHistory => chatHistory.concat(chatEvent))
-            }
-            setWs(ws)
-        }
-
-        return () => {
-            if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
-                console.log('ws cleanup function: closing ws connection')
-                wsRef.current.close()
-                return
-            }
-            console.log('ws cleanup function: no action')
-        }
-    }, [roomId])
-
     return (
         <div className='h-full flex flex-col gap-2 p-4 bg-zinc-800'>
             <h1 className='text-5xl text-white'>Chat</h1>
-            <ChatWindow chatHistory={chatHistory} ws={ws} setRoomId={setRoomId} />
+            <ChatWindow />
         </div>
     )
 }
--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,10 +1,53 @@
-import { ChatWindowProps } from "../types"
+import { useEffect, useRef, useState } from "react";
 import ChatHistory from "./ChatHistory"
 import MessageForm from "./MessageForm"
 import NavigationForm from "./NavigationForm"
+import { ChatEvent } from "../types";
+import { bytesToBase64 } from "../utils";
 
-const ChatWindow = (props: ChatWindowProps) => {
-    const { chatHistory, ws, setRoomId } = props
+const ChatWindow = () => {
+    const [chatHistory, setChatHistory] = useState<ChatEvent[]>([]);
+    const [ws, setWs] = useState<WebSocket | null>(null)
+    const wsRef = useRef<WebSocket | null>(null)
+    const [roomId, setRoomId] = useState("")
+
+    useEffect(() => {
+        console.log('ws useEffect triggered')
+        if (roomId === "") { return }
+        if (!wsRef.current || wsRef.current.readyState === WebSocket.CLOSED || wsRef.current.readyState === WebSocket.CLOSING) {
+            console.log(`Opening a new ws connection to id: ${roomId}`)
+            const ws = new WebSocket(`ws://localhost:8080/${bytesToBase64(new TextEncoder().encode(roomId))}/ws`);
+            wsRef.current = ws
+            wsRef.current.onmessage = (event) => {
+                //const date = new Date(Date.now())
+                //const timestamp = `[${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}]`
+                const chatEvent: ChatEvent = JSON.parse(event.data)
+                console.log(chatEvent)
+                setChatHistory(chatHistory => chatHistory.concat(chatEvent))
+            }
+
+            wsRef.current.onopen = () => {
+                const chatEvent: ChatEvent = { type: "status", payload: 'Connected.' }
+                setChatHistory(chatHistory => chatHistory.concat(chatEvent))
+            }
+
+            wsRef.current.onclose = () => {
+                const chatEvent: ChatEvent = { type: "status", payload: 'Connection closed.' }
+                setChatHistory(chatHistory => chatHistory.concat(chatEvent))
+            }
+            setWs(ws)
+        }
+
+        return () => {
+            if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+                console.log('ws cleanup function: closing ws connection')
+                wsRef.current.close()
+                return
+            }
+            console.log('ws cleanup function: no action')
+        }
+    }, [roomId])
+
     return (
         <div id="chat-window" className='p-2 flex flex-col gap-1 grow border border-zinc-700 rounded-xl'>
             <NavigationForm setRoomId={setRoomId} />
