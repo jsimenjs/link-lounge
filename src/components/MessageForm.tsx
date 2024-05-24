72b057235358b20cf3e8502e442f01a4701f347d
jsimenjs <32468772+jsimenjs@users.noreply.github.com>
2024-05-24 22:08:34 +0200

    refactor: rename variable

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -4,17 +4,17 @@ import { sendMessage } from "../api"
 
 const MessageForm = (props: MessageFormProps) => {
     const { ws } = props
-    const [chatInput, setChatInput] = useState("")
+    const [message, setMessage] = useState("")
 
     const submitHandler = (e: FormEvent) => {
         e.preventDefault()
-        sendMessage(chatInput, ws)
-        setChatInput("")
+        sendMessage(message, ws)
+        setMessage("")
     }
 
     return (
         <form id="message-form" onSubmit={(e) => submitHandler(e)} className='flex flex-row w-full bg-zinc-700 p-2 text-lg rounded-lg'>
-            <input type="text" placeholder='Type a message' value={chatInput} onChange={(e) => { setChatInput(e.target.value) }} className='p-1 bg-zinc-800  placeholder:italic rounded-lg rounded-r-none bg-inherit w-full' />
+            <input type="text" placeholder='Type a message' value={message} onChange={(e) => { setMessage(e.target.value) }} className='p-1 bg-zinc-800  placeholder:italic rounded-lg rounded-r-none bg-inherit w-full' />
             <button type="submit" className='p-0.5 bg-zinc-900 rounded-r-lg text-white px-2'>Send</button>
         </form >
     )
