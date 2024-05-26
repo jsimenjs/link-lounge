40907d3a5d9a39ebcd43c113b6217fa525a8fa3b
jsimenjs <32468772+jsimenjs@users.noreply.github.com>
2024-05-26 21:44:21 +0200

    feat: add search and send button svgs

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -13,10 +13,20 @@ const MessageForm = (props: MessageFormProps) => {
     }
 
     return (
-        <form id="message-form" onSubmit={(e) => submitHandler(e)} className='flex flex-row w-full bg-zinc-700 text-white text-lg rounded-lg'>
-            <input type="text" placeholder='Type a message' value={message} onChange={(e) => { setMessage(e.target.value) }} className='p-1 bg-zinc-800  placeholder:italic rounded-lg rounded-r-none bg-inherit w-full' />
-            <button type="submit" className='p-0.5 bg-zinc-900 rounded-r-lg text-white px-2'>Send</button>
+        <form id="message-form" onSubmit={(e) => submitHandler(e)} className='flex flex-row gap-2 w-full bg-zinc-800 text-white rounded-lg p-2'>
+            <input type="text" placeholder='Type a message' value={message} onChange={(e) => { setMessage(e.target.value) }} className='p-1 bg-zinc-800  placeholder:italic rounded-lg bg-inherit w-full' />
+            <button type="submit" className='bg-zinc-600 rounded-3xl text-white p-2'>
+                <SendSVG />
+            </button>
         </form >
+    )
+}
+
+const SendSVG = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-zinc-400">
+            <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+        </svg>
     )
 }
 
--- src/components/NavigationForm.tsx
+++ src/components/NavigationForm.tsx
@@ -11,10 +11,21 @@ const NavigationForm = (props: NavigationFormProps) => {
     }
 
     return (
-        <form id="navigation-form" onSubmit={navigateHandler} className='z-10 flex flex-row text-white'>
-                <input id="destination" type="text" placeholder="Paste a URL" onChange={(e) => { setDestination(e.target.value) }} className='w-full z-10 placeholder:italic bg-zinc-800 p-1 rounded-lg rounded-r-none'></input>
-                <button className='bg-zinc-900 z-10 p-0.5 px-2 text-white rounded-r-lg'>Go</button>
+        <form id="navigation-form" onSubmit={navigateHandler} className='z-10 bg-zinc-800 rounded-lg p-2 flex flex-row gap-2 text-white'>
+            <input id="destination" type="text" placeholder="Paste a URL" onChange={(e) => { setDestination(e.target.value) }} className='w-full z-10 placeholder:italic bg-inherit p-1 rounded-lg'></input>
+            <button className='bg-zinc-700 z-10 p-2 text-white rounded-3xl'>
+                <SearchSVG />
+            </button>
         </form>
+    )
+}
+
+const SearchSVG = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-zinc-300" >
+            <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+        </svg >
+
     )
 }
 
