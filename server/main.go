2ea25de98428629bed667a8b4bda9b10404b1c01
jsimenjs <32468772+jsimenjs@users.noreply.github.com>
2024-06-11 21:17:59 +0200

    feat: extract manager logic to separate file

--- server/main.go
+++ server/main.go
@@ -1,52 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/jsimenjs/chat-app/middleware"
 )
 
-type Manager struct {
-	hubs []*Hub
-}
-
 func main() {
-    router := http.NewServeMux()
+	router := http.NewServeMux()
 	router.Handle("/", http.FileServer(http.Dir("../dist")))
 
+	manager := NewManager()
+	router.HandleFunc("/{id}/ws", manager.ServeHub)
 
-	manager := Manager{}
-	router.HandleFunc("/{id}/ws", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		for _, hub := range manager.hubs {
-			if id == hub.id {
-				serveWs(hub, w, r)
-				return
-			}
-		}
-		hub := newHub(&manager, id)
-		go hub.run()
-		serveWs(hub, w, r)
-		manager.hubs = append(manager.hubs, hub)
-		fmt.Printf("Created new hub with id: %s\n", hub.id)
-	})
+	stack := middleware.CreateMiddlewareStack(
+		middleware.Logging,
+	)
 
-    stack := middleware.CreateMiddlewareStack(
-        middleware.Logging,
-    )
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: stack(router),
+	}
 
-    server := http.Server {
-        Addr: ":8080",
-        Handler: stack(router),
-    }
-
-    server.ListenAndServe()
-}
-
-func (m *Manager) print() {
-		fmt.Println("Hubs:")
-		for i, hub := range m.hubs {
-			fmt.Printf("%d: %s\n", i, hub.toString())
-		}
+	server.ListenAndServe()
 }
---
+++ server/manager.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Manager struct {
+	hubs []*Hub
+}
+
+func NewManager() Manager {
+    return Manager{}
+}
+
+func (manager *Manager) ServeHub(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		for _, hub := range manager.hubs {
+			if id == hub.id {
+				serveWs(hub, w, r)
+				return
+			}
+		}
+		hub := newHub(manager, id)
+		go hub.run()
+		serveWs(hub, w, r)
+		manager.hubs = append(manager.hubs, hub)
+		log.Printf("Created new hub with id: %s\n", hub.id)
+}
+
+func (m *Manager) print() {
+	fmt.Println("Hubs:")
+	for i, hub := range m.hubs {
+		fmt.Printf("%d: %s\n", i, hub.toString())
+	}
+}
